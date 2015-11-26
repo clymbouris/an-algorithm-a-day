4113dedbd4a97f281026da2e283f536446220acc
d3moid <d3moid@users.noreply.github.com>
2015-11-26 19:25:57 +0200

    Slasher Flick.

--- bonfires.js
+++ bonfires.js
@@ -1,4 +1,18 @@
 /* # freeCodeCamp bonfires */
+/*
+Bonfire: Slasher Flick
+Return the remaining elements of an array after chopping off n elements from the head.
+
+The head meaning the beginning of the array, or the zeroth index
+*/
+
+function slasher(arr, howMany) {
+  // it doesn't always pay to be first
+  arr.splice(0, howMany)
+  return arr;
+}
+
+slasher([1, 2, 3], 2);
 
 /*
 Bonfire: Chunky Monkey
