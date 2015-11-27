4e96fcd1e607434c36c172241d9caff55de2c5bd
d3moid <d3moid@users.noreply.github.com>
2015-11-27 10:35:26 +0200

    Where do I belong.

--- bonfires.js
+++ bonfires.js
@@ -1,6 +1,30 @@
 /* # freeCodeCamp bonfires */
 
 /*
+Bonfire: Where do I belong
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument)
+once it has been sorted.
+
+For example, where([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+Likewise, where([20,3,5], 19) should return 2 because once the array has been sorted
+it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+*/
+
+function where(arr, num) {
+  arr.push(num);
+  arr.sort(function(a, b) {
+	return a - b;
+  });
+  return arr.indexOf(num);
+}
+
+where([40, 60], 50);
+
+
+
+/*
 Bonfire: Seek and Destroy
 You will be provided with an initial array (the first argument in the destroyer function),
 followed by one or more arguments. Remove all elements from the initial array that are of
