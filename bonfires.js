1d73d910241c1b75a6118e0086323f963b4527e7
d3moid <d3moid@users.noreply.github.com>
2015-11-28 18:50:24 +0200

    Roman Numeral Converter

--- bonfires.js
+++ bonfires.js
@@ -1,6 +1,40 @@
 /* # freeCodeCamp bonfires */
 
 /*
+Bonfire: Roman Numeral Converter
+Convert the given number into a roman numeral.
+
+All roman numerals answers should be provided in upper-case.
+
+*/
+
+function convert(num) {
+	var romans = {
+		"M": 1000,
+		"CM": 900,
+		"D": 500,
+		"CD": 400,
+		"C": 100,
+		"XC": 90,
+		"L": 50,
+		"XL": 40,
+		"X": 10,
+		"IX": 9,
+		"V": 5,
+		"IV": 4,
+		"I": 1
+	};
+	var result = "", i;
+	for(i in romans) {
+		while(num >= romans[i]) {
+			result = result + i;
+			num = num - romans[i];
+		}
+	}
+	return result;
+}
+
+/*
 Bonfire: Diff Two Arrays
 Compare two arrays and return a new array with any items only found in one of the original arrays.
 */
