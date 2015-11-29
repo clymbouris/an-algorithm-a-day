f176c796dd0b4001a6764a878cbe7943e337c15b
d3moid <d3moid@users.noreply.github.com>
2015-11-29 10:00:25 +0200

    Where art thou.

--- bonfires.js
+++ bonfires.js
@@ -1,6 +1,38 @@
 /* # freeCodeCamp bonfires */
 
 /*
+Bonfire: Where art thou
+Make a function that looks through an array of objects (first argument)
+and returns an array of all objects that have matching property and value pairs (second argument).
+Each property and value pair of the source object has to be present
+in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is
+[{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }],
+and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument),
+because it contains the property and it's value, that was passed on as the second argument.
+*/
+
+function where(collection, source) {
+	var arr = [];
+	var keys = Object.keys(source);
+	collection.forEach(function(obj) {
+		var i, matches = 0;
+		for(i = 0; i < keys.length; i++) {
+			if(obj.hasOwnProperty(keys[i]) && obj[keys[i]] === source[keys[i]]) {
+				matches++;
+			}
+		}
+		if (matches === keys.length) {
+			arr.push(obj);
+		}
+	});
+	return arr;
+}
+
+where([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+/*
 Bonfire: Roman Numeral Converter
 Convert the given number into a roman numeral.
 
