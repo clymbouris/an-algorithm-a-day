ae9fdc35d93002e70dfe5cb4cca382887e5d3404
d3moid <d3moid@users.noreply.github.com>
2015-12-02 18:16:26 +0200

    Sorted Union.

--- bonfires.js
+++ bonfires.js
@@ -1,6 +1,37 @@
 /* # freeCodeCamp bonfires */
 
 /*
+Bonfire: Sorted Union
+Write a function that takes two or more arrays and returns a new array of unique values
+in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order,
+but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order,
+but the final array should not be sorted in numerical order.
+*/
+
+function unite(arr) {
+	var arglen = arguments.length, arrays = [], result, i;
+	for (i = 0; i < arglen; i++) {
+		arrays.push(arguments[i]);
+	}
+	result = arrays.reduce(function(arr1, arr2) {
+		arr1.forEach(function(elem) {
+			var index = arr2.indexOf(elem);
+			if (index !== -1) {
+				arr2.splice(index, 1);
+			}
+		});
+		return arr1.concat(arr2);
+	});
+	return result;
+}
+
+unite([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+/*
 Bonfire: Boo who
 Check if a value is classified as a boolean primitive. Return true or false.
 
