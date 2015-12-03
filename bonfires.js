53acc6dce695b4a912acc4763ac867099173a24b
d3moid <d3moid@users.noreply.github.com>
2015-12-03 07:10:06 +0200

    Sum All Odd Fibonacci Numbers.

--- bonfires.js
+++ bonfires.js
@@ -1,6 +1,36 @@
 /* # freeCodeCamp bonfires */
 
 /*
+Bonfire: Sum All Odd Fibonacci Numbers
+Return the sum of all odd Fibonacci numbers up to and including the passed number if it is a Fibonacci number.
+
+The first few numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8,
+and each subsequent number is the sum of the previous two numbers.
+
+As an example, passing 4 to the function should return 5 because all the odd Fibonacci numbers under 4 are 1, 1, and 3.
+*/
+
+function sumFibs(num) {
+	function generateFib(max) {
+		var fib = [0, 1],
+		next, i;
+		while (fib[fib.length - 1] + fib[fib.length - 2] <= max) {
+			fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+		}
+		return fib;
+	}
+	var fibonacci = generateFib(num);
+	var odds = fibonacci.filter(function(val) {
+		return (val % 2) ? true : false;
+	});
+	return odds.reduce(function(a, b) {
+		return a + b;
+	});
+}
+
+sumFibs(4);
+
+/*
 Bonfire: Spinal Tap Case
 Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 */
