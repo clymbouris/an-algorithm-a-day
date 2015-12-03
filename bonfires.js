0e54fcd687228d1123062f2f3ea9fdc56da95bef
d3moid <d3moid@users.noreply.github.com>
2015-12-03 20:31:01 +0200

    Sum All Primes.

--- bonfires.js
+++ bonfires.js
@@ -1,6 +1,34 @@
 /* # freeCodeCamp bonfires */
 
 /*
+Bonfire: Sum All Primes
+Sum all the prime numbers up to and including the provided number.
+
+A prime number is defined as having only two divisors, 1 and itself.
+For example, 2 is a prime number because it's only divisible by 1 and 2.
+1 isn't a prime number, because it's only divisible by itself.
+
+The provided number may not be a prime.
+*/
+function sumPrimes(num) {
+	function isPrime(num) {
+		var i;
+		for (i = 2; i < num; i++) {
+			if (num % i === 0) {
+				return false;
+			}
+		}
+		// 1 and negative numbers are not primes
+		return num > 1;
+	}
+	var result = 0, i;
+	for (i = num; i > 1; i--) {
+		if(isPrime(i)) result = result + i;
+	}
+	return result;
+}
+
+/*
 Bonfire: Sum All Odd Fibonacci Numbers
 Return the sum of all odd Fibonacci numbers up to and including the passed number if it is a Fibonacci number.
 
