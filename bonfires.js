50373b8b63f0809d1bb3d85d27dedfb244bc8b63
d3moid <d3moid@users.noreply.github.com>
2015-12-03 21:20:31 +0200

    Finders Keepers.

--- bonfires.js
+++ bonfires.js
@@ -1,6 +1,21 @@
 /* # freeCodeCamp bonfires */
 
 /*
+Bonfire: Finders Keepers
+Create a function that looks through an array (first argument) and returns the first element
+in the array that passes a truth test (second argument).
+
+*/
+
+function find(arr, func) {
+	return arr.filter(func)[0];
+}
+
+find([1, 2, 3, 4], function(num) {
+	return num % 2 === 0;
+});
+
+/*
 Bonfire: Sum All Primes
 Sum all the prime numbers up to and including the provided number.
 
