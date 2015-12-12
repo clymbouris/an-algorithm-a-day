6d905b0df0adb05037b9d86d8086b73183913c08
d3moid <d3moid@users.noreply.github.com>
2015-12-12 08:20:34 +0200

    10001st prime.

--- project_euler.js
+++ project_euler.js
@@ -1,4 +1,32 @@
 /*
+10001st prime
+Problem 7
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+
+function getPrime(pos) {
+	function isPrime(num) {
+		var i;
+		for (i = 2; i < num / 2; i ++) {
+			if( num % i === 0) return false;
+		}
+		return num > 1;
+	}
+	function fetchPrime(pos) {
+		var primes = [], i;
+		for (i = 2; primes.length <= pos; i++) {
+			if(isPrime(i)) primes.push(i);
+		}
+		return primes[primes.length - 1];
+	}
+	return fetchPrime(pos);
+}
+
+getPrime(10001); // 104743
+
+/*
 Sum square difference
 Problem 6
 The sum of the squares of the first ten natural numbers is,
