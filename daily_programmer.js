32808e7e282adced99ce89cd97ca9dba1b22e2cd
d3moid <d3moid@users.noreply.github.com>
2015-12-20 09:56:28 +0200

    Typoglycemia.

--- daily_programmer.js
+++ daily_programmer.js
@@ -1,4 +1,90 @@
 /*
+
+97
+[2015-11-09] Challenge #240 [Easy] Typoglycemia
+submitted 1 month ago * by G33kDude1 1
+Description
+
+Typoglycemia is a relatively new word given to a purported recent discovery about how people read written text.
+As wikipedia puts it:
+The legend, propagated by email and message boards, purportedly demonstrates that readers can understand the
+meaning of words in a sentence even when the interior letters of each word are scrambled.
+As long as all the necessary letters are present, and the first and last letters remain the same, readers
+appear to have little trouble reading the text.
+
+Or as Urban Dictionary puts it:
+Typoglycemia
+The mind's ability to decipher a mis-spelled word if the first and last letters of the word are correct.
+The word Typoglycemia describes Teh mdin's atbiliy to dpeihecr a msi-selpeld wrod if the fsirt and lsat lteetrs of the wrod are cerorct.
+
+Input Description
+Any string of words with/without punctuation.
+
+Output Description
+A scrambled form of the same sentence but with the word's first and last letter's positions intact.
+Sample Inputs
+
+According to a research team at Cambridge University, it doesn't matter in what order the letters in a word are, 
+the only important thing is that the first and last letter be in the right place. 
+The rest can be a total mess and you can still read it without a problem.
+This is because the human mind does not read every letter by itself, but the word as a whole. 
+Such a condition is appropriately called Typoglycemia.
+Sample Outputs
+
+Aoccdrnig to a rseearch taem at Cmabrigde Uinervtisy, it deosn't mttaer in waht oredr the ltteers in a wrod are, 
+the olny iprmoatnt tihng is taht the frist and lsat ltteer be in the rghit pclae. 
+The rset can be a taotl mses and you can sitll raed it wouthit a porbelm. 
+Tihs is bcuseae the huamn mnid deos not raed ervey lteter by istlef, but the wrod as a wlohe. 
+Scuh a cdonition is arppoiatrely cllaed Typoglycemia.
+*/
+
+// my attempt
+function typoglycemia(str) {
+	var words = str.split(' ');
+	return words.map(word => {
+		var newWord = word[0], len = word.length - 1;
+		if(len !== 0) {
+			var chars = [], i;
+			for (i = 1; i < len; i++) {
+				if (i % 2 === 0 || word[i] === ',' || word[i] === '.' || word[i] === '\'') {
+					chars.push(word[i]);
+				}
+				else {
+					chars.unshift(word[i]);
+				}
+			}
+			var j;
+			for (j = 0; j < chars.length; j++) {
+				newWord += chars[j];
+			}
+			newWord += word[len];
+		}
+		return newWord;
+	}).join(' ');
+}
+
+// seems much more solid solution (taken from forums)
+function typoglycemia(input){
+    return input.replace(/\b\w+\b/gm,function(word){
+        var w, glyc = word;
+        while (glyc === word && word.length >= 4){
+            w = word.split('');
+            glyc = w.shift() + '';
+            while (w.length > 1) glyc += w.splice(Math.random() * w.length - 1, 1);
+            glyc += w.length ? w[0] : '';
+        }
+        return glyc;
+    });
+}
+
+str = "The legend, propagated by email and message boards," +
+" purportedly demonstrates that readers can understand the meaning" +
+" of words in a sentence even when the interior letters of each word" +
+" are scrambled. As long as all the necessary letters are present, and the" +
+" first and last letters remain the same, readers appear to have little trouble reading the text."
+
+
+/*
 [2015-10-26] Challenge #238 [Easy] Consonants and Vowels
 submitted 1 month ago by jnazario2 0
 Description
